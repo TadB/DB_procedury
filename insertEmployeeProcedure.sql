9b990692685a1b4d525e5eeccd00d585aa2ae700
TadB <tadeusz.brzytwa@gmail.com>
2018-05-26 12:39:49 +0200

    działający exception niewłaściwego managera

--- insertEmployeeProcedure.sql
+++ insertEmployeeProcedure.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE InsertEmployee (
+create or replace PROCEDURE InsertEmployee (
     P_FIRST_NAME IN VARCHAR2,
     P_LAST_NAME IN VARCHAR2,
     P_EMAIL IN VARCHAR2,
@@ -7,19 +7,22 @@ CREATE OR REPLACE PROCEDURE InsertEmployee (
     P_SALARY IN NUMBER,
     P_DEPARTMENT_ID IN NUMBER,
     P_MANAGER_ID IN NUMBER) IS
-    
+
     not_nullable EXCEPTION;
     incorrect_manager_id EXCEPTION;
     incorrect_department_id EXCEPTION;
     incorrect_job_id EXCEPTION;
     incorrect_salary EXCEPTION;
     v_manager_id DEPARTMENTS.MANAGER_ID%TYPE;
+    flag NUMBER(3);
 BEGIN
-    SELECT MANAGER_ID INTO v_manager_id FROM DEPARTMENTS WHERE MANAGER_ID=P_MANAGER_ID;
+    flag := 1;
+    SELECT DISTINCT MANAGER_ID INTO v_manager_id FROM EMPLOYEES WHERE MANAGER_ID=P_MANAGER_ID;
+
     IF (P_HIRE_DATE IS NULL OR P_JOB_ID IS NULL OR P_EMAIL IS NULL OR P_LAST_NAME IS NULL) THEN
         RAISE not_nullable;
-    ELSIF v_manager_id IS NULL THEN
-        RAISE incorrect_manager_id;
+--    ELSIF v_manager_id IS NULL THEN
+--        RAISE incorrect_manager_id;
 --    ELSIF (SELECT DEPARTMENT_ID FROM DEPARTMENTS WHERE DEPARTMENT_ID=P_DEPARTMENT_ID) IS NOT NULL THEN
 --        RAISE incorrect_department_id_exception;
 --    ELSIF (SELECT JOB_ID FROM JOBS WHERE JOB_ID=P_JOB_ID) IS NOT NULL THEN
@@ -35,6 +38,10 @@ EXCEPTION
         dbms_output.put_line('wartość NULL dla pola z warunkiem NOT NULL');
     WHEN incorrect_manager_id THEN
         dbms_output.put_line('niewłaściwy lub nieistniejący kod przełożonego');
+    WHEN NO_DATA_FOUND THEN
+        IF flag = 1 THEN
+            DBMS_OUTPUT.PUT_LINE('niewłaściwy lub nieistniejący kod przełożonego');
+        END IF;
 --    WHEN incorrect_department_id THEN
 --        dbms_output.put_line('niewłaściwy lub nieistniejący kod departamentu');
 --    WHEN incorrect_job_id THEN
@@ -42,6 +49,3 @@ EXCEPTION
 --    WHEN incorrect_salary THEN
 --        dbms_output.put_line('pensja spoza zakresu przysługującego wynagrodzenia dla zajmowanego stanowiska');
 END InsertEmployee;
-
-
-InsertEmployee('Jan', 'Kowalski', 'JKowalsk', '21-JUN-07', 'SH_CLERK', 2600, 124,	50);

a5668766c826c576e7f866d4b854a34772fa453c
TadB <tadeusz.brzytwa@gmail.com>
2018-05-26 14:37:38 +0200

    dodano obsługę wyjątku niepoprawnej daty dodawania

--- insertEmployeeProcedure.sql
+++ insertEmployeeProcedure.sql
@@ -10,11 +10,13 @@ create or replace PROCEDURE InsertEmployee (
 
     not_nullable EXCEPTION;
     incorrect_salary EXCEPTION;
+    further_hire_date EXCEPTION;
     v_manager_id DEPARTMENTS.MANAGER_ID%TYPE;
     v_department_id DEPARTMENTS.DEPARTMENT_ID%TYPE;
     v_max_salary JOBS.MAX_SALARY%TYPE;
     v_min_salary JOBS.MIN_SALARY%TYPE;
     v_job_id JOBS.JOB_ID%TYPE;
+    v_current_date EMPLOYEES.HIRE_DATE%TYPE;
     flag NUMBER(3);
 BEGIN
     flag := 1;
@@ -27,10 +29,13 @@ BEGIN
     SELECT MAX_SALARY INTO v_max_salary FROM JOBS WHERE JOB_ID = P_JOB_ID;
     SELECT MIN_SALARY INTO v_min_salary FROM JOBS WHERE JOB_ID = P_JOB_ID;
 
+    SELECT CURRENT_DATE INTO v_current_date FROM DUAL;
     IF (P_HIRE_DATE IS NULL OR P_JOB_ID IS NULL OR P_EMAIL IS NULL OR P_LAST_NAME IS NULL) THEN
         RAISE not_nullable;
     ELSIF (v_max_salary<P_SALARY OR v_min_salary>P_SALARY) THEN
        RAISE incorrect_salary;
+    ELSIF P_HIRE_DATE > v_current_date THEN
+        RAISE further_hire_date;
     END IF;
     INSERT INTO EMPLOYEES(EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, HIRE_DATE, JOB_ID, SALARY, DEPARTMENT_ID, MANAGER_ID)
     VALUES (SEQ_EMP_ID.NEXTVAL, P_FIRST_NAME, P_LAST_NAME, P_EMAIL, P_HIRE_DATE, P_JOB_ID, P_SALARY, P_DEPARTMENT_ID, P_MANAGER_ID);
@@ -47,8 +52,6 @@ EXCEPTION
         END IF;
     WHEN incorrect_salary THEN
         dbms_output.put_line('pensja spoza zakresu przysługującego wynagrodzenia dla zajmowanego stanowiska');
---    WHEN incorrect_department_id THEN
---        dbms_output.put_line('niewłaściwy lub nieistniejący kod departamentu');
---    WHEN incorrect_job_id THEN
---        dbms_output.put_line('niewłaściwy lub nieistniejący kod stanowiska');
+    WHEN further_hire_date THEN
+        dbms_output.put_line('data zatrudnienia jest wcześniejsza niż data dzisiejsza');
 END InsertEmployee;
--- testy.sql
+++ testy.sql
@@ -2,25 +2,29 @@
 SET SERVEROUTPUT ON;
 --formula poprawna
 begin
-insertEmployee('Marcin', 'Testowy', 'MTESTOWY', '17-SEP-18', 'AD_VP', 6000, 100, 50);
+insertEmployee('Marcin', 'Testowy', 'MTESTOWY', '17-SEP-11', 'AD_VP', 20000, 100, 50);
 end;
 --formula ze zlym departamentem
 BEGIN
-insertEmployee('Marcin', 'Testowy', 'MTESTOWY', '17-SEP-18', 'AD_VP', 6000, 100, 1);
+insertEmployee('Marcin', 'Testowy', 'MTESTOWY', '17-SEP-11', 'AD_VP', 20000, 100, 1);
 END;
 --formula ze zlym managerem
 BEGIN
-insertEmployee('Marcin', 'Testowy', 'MTESTOWY', '17-SEP-18', 'AD_VP', 6000, 1, 50);
+insertEmployee('Marcin', 'Testowy', 'MTESTOWY', '17-SEP-11', 'AD_VP', 20000, 1, 50);
 END;
 --formula ze zlym kodem stanowiska
 BEGIN
-insertEmployee('Marcin', 'Testowy', 'MTESTOWY', '17-SEP-18', 'NIIIIC', 6000, 100, 50);
+insertEmployee('Marcin', 'Testowy', 'MTESTOWY', '17-SEP-11', 'NIIIIC', 20000, 100, 50);
 END;
 --formula ze zbyt dużą pensją
 BEGIN
-insertEmployee('Marcin', 'Testowy', 'MTESTOWY', '17-SEP-18', 'AD_VP', 1000000, 100, 50);
+insertEmployee('Marcin', 'Testowy', 'MTESTOWY', '17-SEP-11', 'AD_VP', 1000000, 100, 50);
 END;
 --formula ze zbyt małą pensją
 BEGIN
-insertEmployee('Marcin', 'Testowy', 'MTESTOWY', '17-SEP-18', 'AD_VP', 7, 100, 50);
+insertEmployee('Marcin', 'Testowy', 'MTESTOWY', '17-SEP-11', 'AD_VP', 7, 100, 50);
+END;
+--formula ze złą data zatrudnienia
+BEGIN
+insertEmployee('Marcin', 'Testowy', 'MTESTOWY', '17-SEP-33', 'AD_VP', 20000, 100, 50);
 END;
